0e40d9d0c335aae079ebc4cf2b2030b540dfc350
Vaishk2004 <vaishalikanadje1@gmail.com>
2025-07-21 14:47:44 +0530

    Delete Assingment3/CheckIdPassword.py

--- Assingment3/CheckIdPassword.py
+++
@@ -1,9 +0,0 @@
-# Check if user has entered correct userid and password
-userid = input("Enter the Userid: ")
-password = input("Enter the Password: ")
-
-if(userid == "Vaishali" and  password == "Vaishali123"):
-    print("Valid!! Successfully login.")
-
-else:
-    print("Invalid userid and password.")
